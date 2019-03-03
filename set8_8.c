40e1dc8e3ed067cec38758c1b02de71de7682eee
thalaraj <47813910+thalaraj@users.noreply.github.com>
2019-03-03 00:54:18 -0800

    Create set8_8.c

---
+++ set8_8.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+void main()
+{
+    int a;
+    scanf("%d",&a);
+      if(a%13==0)
+      
+      
+        {
+            printf("yes");
+        }
+        else
+        {
+            printf("no");
+        }
+    
+}
